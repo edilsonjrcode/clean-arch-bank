752b80c9f5a4a2d3d2356a0b5d61da8f9f99353a
edilsonjrcode <2022111510116@iesp.edu.br>
2025-10-25 14:37:33 -0300

    fix: add more params in create method

--- usecase/src/main/java/br/com/jrcode/usecase/CreateUserUseCase.java
+++ usecase/src/main/java/br/com/jrcode/usecase/CreateUserUseCase.java
@@ -2,8 +2,10 @@ package br.com.jrcode.usecase;
 
 import br.com.jrcode.core.domain.User;
 import br.com.jrcode.core.exception.EmailException;
+import br.com.jrcode.core.exception.InternalServerErrorException;
 import br.com.jrcode.core.exception.TaxNumberException;
+import br.com.jrcode.core.exception.TransactionPinException;
 
 public interface CreateUserUseCase {
-    void create(User user) throws TaxNumberException, EmailException;
+    void create(User user, String pin) throws TaxNumberException, EmailException, TransactionPinException, InternalServerErrorException;
 }
